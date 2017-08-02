47fe28242133022b85035f21f6070d749befc826
Руслан Коркмазов <rouslan.korkmazov@gmail.com>
2017-08-02 11:52:50 +0300

    user_name -> $USER

--- zshrc_init.sh
+++ zshrc_init.sh
@@ -1,6 +1,6 @@
-alias skazka='cd /Users/marvin/Documents/projects/skazka && vagrant up && vagrant ssh'
-alias skazka_suspend='cd /Users/marvin/Documents/projects/skazka && vagrant suspend'
-alias minecraft='java -jar /Users/marvin/Applications/TLauncher.jar'
+alias skazka='cd /Users/$USER/Documents/projects/skazka && vagrant up && vagrant ssh'
+alias skazka_suspend='cd /Users/$USER/Documents/projects/skazka && vagrant suspend'
+alias minecraft='java -jar /Users/$USER/Applications/TLauncher.jar'
 
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
@@ -12,7 +12,7 @@ alias indexingProject="ctags -R --fields=+l --languages=python --python-kinds=-i
 
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
 source /usr/local/bin/virtualenvwrapper.sh
-export WORKON_HOME="/Users/marvin/envs/"
+export WORKON_HOME="/Users/$USER/envs/"
 
 setopt menucomplete
 zstyle ':completion:*' menu select=1 _complete _ignored _approximate
